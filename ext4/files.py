22603d0847193f7918901f5b51e4eff3891146d0
Henry-Joseph Audéoud <henry-joseph.audeoud@univ-grenoble-alpes.fr>
2020-12-15 17:27:12 +0100

    Add licence

--- ext4/files.py
+++ ext4/files.py
@@ -1,3 +1,21 @@
+# Copyright 2020 Henry-Joseph Audéoud & Timothy Claeys
+#
+# This file is part of ext4-reader.
+#
+# ext4-reader is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ext4-reader is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with ext4-reader.  If not, see
+# <https://www.gnu.org/licenses/>.
+
 import abc
 from typing import Optional, Iterator
 
